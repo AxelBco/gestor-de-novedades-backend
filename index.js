b075b29168960aeb46a11be518931c92ad3d3031
AxelBco <axel.ezequiel123@outlook.com>
2025-08-03 20:29:18 -0300

    corrijo backend para render

--- index.js
+++ index.js
@@ -20,24 +20,32 @@ const ActividadSchema = new mongoose.Schema({
 const Actividad = mongoose.model('Actividad', ActividadSchema);
 
 // Endpoints
+app.get('/', (req, res) => {
+  res.send('API funcionando');
+});
+
 app.get('/actividades', async (req, res) => {
   const actividades = await Actividad.find();
   res.json(actividades);
 });
 
-app.put ('/actividades/:id', async (req, res) => {
+app.put('/actividades/:id', async (req, res) => {
   const actividadActualizada = await Actividad.findByIdAndUpdate(req.params.id, req.body, { new: true });
-  res.json(actividadActualizada); });
+  res.json(actividadActualizada);
+});
 
 app.post('/actividades', async (req, res) => {
-  console.log('POST /actividades body:', req.body); // Debes ver detalles aquí
+  console.log('POST /actividades body:', req.body);
   const nueva = new Actividad(req.body);
   await nueva.save();
   res.json(nueva);
 });
+
 app.delete('/actividades/:id', async (req, res) => {
   await Actividad.findByIdAndDelete(req.params.id);
   res.json({ ok: true });
 });
 
-app.listen(4000, () => console.log('Servidor backend en puerto 4000'));
+// Usar el puerto asignado por Render
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => console.log(`Servidor backend en puerto ${PORT}`));
